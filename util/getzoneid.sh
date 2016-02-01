2a5cd0d6ecae9ea2187709363b9be5bbb2e6e530
grant haywood <grant@iowntheinter.net>
2016-02-01 19:19:55 +0000

    fixes

--- util/getzoneid.sh
+++ util/getzoneid.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws route53 list-hosted-zones | jq -c '.HostedZones[]|select(.Name=="$1.") | {Id}' | cut -d '"' -f 4
+aws route53 list-hosted-zones | jq -c -r '.HostedZones[]|select(.Name=="'$1.'").Id' 
